635e3c9d1e93725de74c98ab1faec481bcbe26c2
Binay Kumar Sahoo <binay.sahoo2013@gmail.com>
2021-06-12 21:44:38 +0530

    added hosted client url

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const io = require('socket.io')(process.env.PORT || 5000, {
   cors: {
-    origin: 'http://localhost:3000',
+    origins: [
+      'http://localhost:3000',
+      'https://binaykumarsahoo.github.io/whatsapp-clone-client',
+    ],
     methods: ['GET', 'POST'],
   },
 });
